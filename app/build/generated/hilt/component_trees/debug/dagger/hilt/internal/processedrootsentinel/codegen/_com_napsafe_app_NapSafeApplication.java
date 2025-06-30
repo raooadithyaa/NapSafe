968b17da0d7071d2e810e63c1c3f3331817c5391
adithya rao <adithya.rao@inmobi.com>
2025-06-30 21:11:52 +0530

    > Cleanup: remove build, generated, and cache files from repository

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_napsafe_app_NapSafeApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.napsafe.app.NapSafeApplication"
-)
-public final class _com_napsafe_app_NapSafeApplication {
-}
--- app/build/tmp/kapt3/classes/debug/META-INF/proguard/com_napsafe_app_ui_screens_alarms_AlarmsViewModel_HiltModules_KeyModule_LazyClassKeys.pro
+++
@@ -1,1 +0,0 @@
--keep,allowobfuscation,allowshrinking class com.napsafe.app.ui.screens.alarms.AlarmsViewModel
